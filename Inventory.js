675e373a16dd43235f363238046fdefc8cf248cb
john <rrivers52@gmail.com>
2024-11-14 18:13:52 -0500

    first commit

---
+++ Inventory.js
@@ -0,0 +1,39 @@
+// let css_room_inventory = ["dog","cat"];
+
+// for (let i=0; i<css_room_inventory.length; i++)
+//     { document.write(`
+//      <tr>
+        
+//             <td>${i+1}</td>
+//             <td>${css_room_inventory[i]}</td>
+            
+        
+//     </tr>
+//     `)
+    
+
+//     }
+// let School_invetory = [""];
+
+// for (let i=0; i<School_invetory.length; i++){
+//     document.getElementById (`
+        
+//         <tr>
+//             <td>${i +1}</td>
+//             <td>${School_invetory[i]}</td>
+//         </tr>
+//         `)
+
+
+// }
+let intke_1 = document.getElementById("submit_1");
+let btn_1 = document.getElementById("input_button");
+let usbc_1 = document.getElementById("usb-C_chargers");
+
+function add_to_chargers_1(){
+    for (let i=0; )
+    
+
+}
+    btn_1.addEventListener('click', add_to_chargers_1);
+
