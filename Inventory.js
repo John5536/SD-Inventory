03cc54568ada2be85a7eb96fd0bd852dc87f7db2
John5536 <rrivers52@gmail.com>
2024-11-14 19:13:45 -0500

    second commit

--- Inventory.js
+++ Inventory.js
@@ -29,11 +29,19 @@
 let intke_1 = document.getElementById("submit_1");
 let btn_1 = document.getElementById("input_button");
 let usbc_1 = document.getElementById("usb-C_chargers");
-
+let charger_USBC = []
 function add_to_chargers_1(){
-    for (let i=0; )
+    charger_USBC.push(document.getElementById("submit_1").value);
     
-
 }
+for ( let i=0; i<charger_USBC.length; i++){(`
+    
+    <div>
+        <p>${i+1} ${charger_USBC[1]}</p>
+    </div>
+    
+`)
+}
+
     btn_1.addEventListener('click', add_to_chargers_1);
 
